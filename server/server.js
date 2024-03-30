4f9a06e0cd4dc7e044cf195d251e6ba5d342b399
emanuelefavero <emafaux89@gmail.com>
2024-03-30 18:11:36 +0100

    add server code and install vite react-ts client

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const cors = require('cors')
+
+const app = express()
+
+const clientUrl = process.env.development
+  ? 'http://localhost:3000'
+  : 'https://YOUR-APP.com' // ? make sure to change this to your production URL
+
+app.use(
+  cors({
+    origin: clientUrl,
+  })
+)
+
+const server = http.createServer(app)
+const io = socketIo(server, {
+  cors: {
+    origin: clientUrl,
+    // methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message) // send message to all connected clients
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+})
+
+const PORT = process.env.PORT || 4000
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

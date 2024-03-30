87f0c03333965f63465e8b21232271ff21c5db1d
emanuelefavero <emafaux89@gmail.com>
2024-03-30 18:41:26 +0100

    add socket.io to the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import { useEffect } from 'react'
+import io from 'socket.io-client'
+
+// TODO check for dev or prod, add proper server url depending on that @see https://vitejs.dev/guide/env-and-mode
+const serverUrl = 'http://localhost:4000'
+
+const socket = io(serverUrl)
 
 function App() {
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server')
+    })
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server')
+    })
+  }, [])
+
   return (
     <>
-      <h1 className='text-purple-500'>Hello</h1>
+      <h1 className='text-purple-500'>Secret Chat</h1>
     </>
   )
 }

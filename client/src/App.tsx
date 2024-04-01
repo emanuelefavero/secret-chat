4bd090df87d025af90cb1591b87862a68e11ad18
emanuelefavero <emafaux89@gmail.com>
2024-04-02 01:36:10 +0200

    add userId to each session instance

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import io from 'socket.io-client'
+import { v4 as uuidv4 } from 'uuid'
 
 // TODO check for dev or prod, add proper server url depending on that @see https://vitejs.dev/guide/env-and-mode
 const serverUrl = 'http://localhost:4000'
@@ -8,33 +9,60 @@ const socket = io(serverUrl)
 
 // --- Message Component ---
 interface MessageProps {
-  username: string
   text: string
+  isCurrentUser: boolean
 }
 
-function Message({ username, text }: MessageProps) {
+function Message({ text, isCurrentUser }: MessageProps) {
+  const currentUserMessageStyle = isCurrentUser ? 'bg-blue-500' : ''
+
   return (
-    <>
-      <p>{username}</p>
+    <div className={`${currentUserMessageStyle}`}>
       <p>{text}</p>
-    </>
+    </div>
   )
 }
 // -------------------------
 
+interface Message {
+  userId: string
+  text: string
+}
+
 function App() {
-  const [messages, setMessages] = useState<MessageProps[]>([])
+  const [userId, setUserId] = useState('')
+  const [messages, setMessages] = useState<Message[]>([])
   const [messageText, setMessageText] = useState('')
 
   useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages([...messages, message])
-    })
-  }, [messages])
+    // Generate a random user ID and save it in session storage
+    let id = sessionStorage.getItem('userId')
+    if (!id) {
+      id = uuidv4()
+      sessionStorage.setItem('userId', id)
+    }
+    setUserId(id)
+
+    // Add a listener to receive messages
+    const messageListener = (message: Message) => {
+      setMessages((prevMessages) => [...prevMessages, message])
+    }
+
+    // Listen for messages
+    socket.on('message', messageListener)
+
+    // Cleanup function to remove the event listener
+    return () => {
+      socket.off('message', messageListener)
+    }
+  }, [])
 
   const sendMessage = () => {
-    socket.emit('sendMessage', { text: messageText })
-    setMessageText('')
+    if (messageText.trim()) {
+      const message = { userId, text: messageText }
+      socket.emit('sendMessage', message)
+      setMessageText('')
+    }
   }
 
   return (
@@ -45,8 +73,8 @@ function App() {
         {messages.map((message, index) => (
           <Message
             key={index}
-            username={message.username}
             text={message.text}
+            isCurrentUser={message.userId === userId}
           />
         ))}
       </div>

c5156785e2310100edceb59daa8df1aa481e4d93
emanuelefavero <emafaux89@gmail.com>
2024-04-02 07:38:42 +0200

    add - handleCopyUserId in Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,22 @@ interface Props {
 }
 
 export default function Header({ userId }: Props) {
+  const handleCopyUserId = () => {
+    navigator.clipboard.writeText(userId) // copy to clipboard
+  }
+
   return (
     <header>
       <h1 className='text-sm'>Secret Chat</h1>
 
       <p className='text-sm text-gray-500 flex flex-col'>
-        Your user ID is: <code className='text-green-500'>{userId}</code>
+        Your user ID is:{' '}
+        <button
+          onClick={handleCopyUserId}
+          className='bg-transparent px-0 w-max'
+        >
+          <code className='text-green-500'>{userId}</code>
+        </button>
       </p>
     </header>
   )

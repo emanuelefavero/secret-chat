fdb5a0f8206d7cc4e2b9b2f4c60d042f28e475e1
emanuelefavero <emafaux89@gmail.com>
2024-04-02 08:08:23 +0200

    add - copied tooltip in Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,24 +1,39 @@
+import { useState } from 'react'
+import './Header.css'
+
 interface Props {
   userId: string
 }
 
 export default function Header({ userId }: Props) {
+  const [isCopied, setIsCopied] = useState(false)
+
   const handleCopyUserId = () => {
     navigator.clipboard.writeText(userId) // copy to clipboard
+    setIsCopied(true) // show copied message
+    setTimeout(() => setIsCopied(false), 1500) // reset after 1.5 seconds
   }
 
   return (
     <header>
       <h1 className='text-sm'>Secret Chat</h1>
 
-      <p className='text-sm text-gray-500 flex flex-col'>
+      <p className='text-sm text-gray-500 flex flex-col max-w-fit'>
         Your user ID is:{' '}
-        <button
-          onClick={handleCopyUserId}
-          className='bg-transparent px-0 w-max'
-        >
-          <code className='text-green-500'>{userId}</code>
-        </button>
+        <div className='relative flex flex-col items-center'>
+          <button
+            onClick={handleCopyUserId}
+            className='bg-transparent px-0 w-max'
+          >
+            <code className='text-green-500'>{userId}</code>
+          </button>
+
+          {isCopied && (
+            <span className='user-id-copied-tooltip absolute -bottom-8 mb-2 w-fit text-xs text-black dark:text-white bg-gray-300 dark:bg-gray-900 px-2 py-1 rounded-md'>
+              Copied!
+            </span>
+          )}
+        </div>
       </p>
     </header>
   )

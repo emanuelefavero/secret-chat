e5b78aec4084658e60c789ab77de89aeb241f85b
emanuelefavero <emafaux89@gmail.com>
2024-04-03 17:57:18 +0200

    update header with logo icon

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './Header.css'
+import logoIcon from '/logo.svg'
 
 interface Props {
   userId: string
@@ -15,26 +16,33 @@ export default function Header({ userId }: Props) {
   }
 
   return (
-    <header className='fixed w-full py-4 px-4 border-b border-gray-300 dark:border-gray-800'>
-      <h1 className='text-sm'>Secret Chat</h1>
+    <header className='fixed w-full px-2 py-2 border-b border-gray-300 dark:border-gray-800'>
+      <div className='flex items-center'>
+        <h1 className='text-sm'>
+          <img
+            src={logoIcon}
+            alt='Logo'
+            className='w-8 h-8 inline-block mr-3'
+          />
+        </h1>
+        <p className='text-sm text-gray-500 flex flex-col max-w-fit'>
+          Your user ID is:{' '}
+          <div className='relative flex flex-col items-center'>
+            <button
+              onClick={handleCopyUserId}
+              className='text-green-500 bg-transparent px-0 w-max hover:text-green-400 hover:underline active:scale-95 transition-all duration-200 ease-in'
+            >
+              <code>{userId}</code>
+            </button>
 
-      <p className='text-sm text-gray-500 flex flex-col max-w-fit'>
-        Your user ID is:{' '}
-        <div className='relative flex flex-col items-center'>
-          <button
-            onClick={handleCopyUserId}
-            className='text-green-500 bg-transparent px-0 w-max hover:text-green-400 active:scale-95 transition-all duration-200 ease-in'
-          >
-            <code>{userId}</code>
-          </button>
-
-          {isCopied && (
-            <span className='user-id-copied-tooltip absolute -bottom-8 mb-2 w-fit text-xs text-black dark:text-white bg-gray-300 dark:bg-gray-900 px-2 py-1 rounded-md'>
-              Copied!
-            </span>
-          )}
-        </div>
-      </p>
+            {isCopied && (
+              <span className='user-id-copied-tooltip absolute -bottom-8 mb-2 w-fit text-xs text-black dark:text-white bg-gray-300 dark:bg-gray-900 px-2 py-1 rounded-md'>
+                Copied!
+              </span>
+            )}
+          </div>
+        </p>
+      </div>
     </header>
   )
 }

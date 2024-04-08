f34d4a4e310ec0a5e73ee30f7e42162cb3878703
emanuelefavero <emafaux89@gmail.com>
2024-04-08 17:30:41 +0200

    add ClearChatButton style

---
+++ client/src/components/ClearChatButton.tsx
@@ -0,0 +1,7 @@
+export default function ClearChatButton() {
+  return (
+    <button className='w-full text-sm rounded-none bg-rose-500 bg-opacity-20 text-rose-500 p-1 border-b border-rose-300 border-opacity-50 hover:bg-opacity-25 active:bg-opacity-30 transition-all duration-150 ease-in'>
+      Clear Chat
+    </button>
+  )
+}
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useChat } from '@/context/useChat'
 import './Header.css'
+import ClearChatButton from '@/components/ClearChatButton'
 import logoIcon from '/logo.svg'
 import CopyIcon from '@/components/icons/CopyIcon'
 
@@ -15,39 +16,42 @@ export default function Header() {
   }
 
   return (
-    <header className='fixed z-40 w-full px-2 py-2 border-b border-gray-300 dark:border-gray-800'>
-      <div className='flex items-center'>
-        <h1 className='text-sm'>
-          <img
-            src={logoIcon}
-            alt='Logo'
-            className='w-8 h-8 inline-block ml-1 mr-3'
-          />
-        </h1>
-        <div className='text-sm text-gray-500 flex flex-col max-w-fit'>
-          Your user ID is:{' '}
-          <div className='relative flex flex-col items-center'>
-            <button
-              onClick={handleCopyUserId}
-              className='flex text-green-500 bg-transparent px-0 w-max hover:text-green-400 hover:underline active:scale-95 transition-all duration-200 ease-in'
-            >
-              <code>{userId}</code>
+    <header className='fixed w-full z-40'>
+      <div className='px-2 py-2 border-b border-gray-300 dark:border-gray-800'>
+        <div className='flex items-center'>
+          <h1 className='text-sm'>
+            <img
+              src={logoIcon}
+              alt='Logo'
+              className='w-8 h-8 inline-block ml-1 mr-3'
+            />
+          </h1>
+          <div className='text-sm text-gray-500 flex flex-col max-w-fit'>
+            Your user ID is:{' '}
+            <div className='relative flex flex-col items-center'>
+              <button
+                onClick={handleCopyUserId}
+                className='flex text-green-500 bg-transparent px-0 w-max hover:text-green-400 hover:underline active:scale-95 transition-all duration-200 ease-in'
+              >
+                <code>{userId}</code>
 
-              {!isCopied && (
-                <span className='w-4 h-4 ml-1 relative top-[1.5px]'>
-                  <CopyIcon strokeColor={isCopied ? '#22c55e' : '#6B7280'} />
+                {!isCopied && (
+                  <span className='w-4 h-4 ml-1 relative top-[1.5px]'>
+                    <CopyIcon strokeColor={isCopied ? '#22c55e' : '#6B7280'} />
+                  </span>
+                )}
+              </button>
+
+              {isCopied && (
+                <span className='user-id-copied-tooltip absolute -bottom-8 mb-2 w-fit text-xs text-black dark:text-white bg-gray-300 dark:bg-gray-900 px-2 py-1 rounded-md'>
+                  Copied!
                 </span>
               )}
-            </button>
-
-            {isCopied && (
-              <span className='user-id-copied-tooltip absolute -bottom-8 mb-2 w-fit text-xs text-black dark:text-white bg-gray-300 dark:bg-gray-900 px-2 py-1 rounded-md'>
-                Copied!
-              </span>
-            )}
+            </div>
           </div>
         </div>
       </div>
+      <ClearChatButton />
     </header>
   )
 }

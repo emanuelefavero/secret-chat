617bc242d599edfe93ed189392cff07fb71e7cb1
emanuelefavero <emafaux89@gmail.com>
2024-04-11 13:20:01 +0200

    fix chat responsive

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -14,8 +14,8 @@ type Ref = HTMLDivElement
 const Message = forwardRef<Ref, Props>(
   ({ text, isCurrentUser }: Props, ref) => {
     const currentUserMessageStyle = isCurrentUser
-      ? 'text-white bg-blue-500 currentUserBubble'
-      : 'text-gray-900 bg-gray-200 dark:text-white dark:bg-gray-900 otherUserBubble'
+      ? 'text-white bg-blue-500 currentUserBubble ml-8'
+      : 'text-gray-900 bg-gray-200 dark:text-white dark:bg-gray-900 mr-8 otherUserBubble'
     const justifyRightIfCurrentUser = isCurrentUser
       ? 'justify-end'
       : 'justify-start'
@@ -24,7 +24,7 @@ const Message = forwardRef<Ref, Props>(
       <div className={`flex ${justifyRightIfCurrentUser} relative`}>
         <div
           ref={ref}
-          className={`${currentUserMessageStyle} mb-2 px-3 py-1 rounded-2xl w-fit max-w-xs relative`}
+          className={`${currentUserMessageStyle} mb-2 px-3 py-1 rounded-2xl w-fit max-w-xl relative`}
         >
           <p>{text}</p>
         </div>
